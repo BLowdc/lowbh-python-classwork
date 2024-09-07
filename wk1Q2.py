bdf063d3246074f73da679e6cba88bb40c0bc990
BLowdc <lowbh@dulwich.org.uk>
2024-09-07 21:34:54 +0100

    commmit

--- wk1Q2.py
+++
@@ -1,7 +0,0 @@
-hrs = int(input())
-min = int(input())
-x = input()
-if x == 'am':
-    print(hrs*60+min)
-elif x == 'pm':
-    print(hrs*60+min+12*60)
--- wk1Q8.py
+++
@@ -1,8 +0,0 @@
-string = input()
-num = ['zero','one','two','three','four','five','six','seven','eight','nine']
-for i in range(len(string)):
-    if string[i] in '0123456789':
-        left = string[:i]
-        right = string[i+1:]
-        string = left + num[int(string[i])] + right
-print(string)
