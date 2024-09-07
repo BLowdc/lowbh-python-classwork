33d860b32501f532d2ad06d4679a350f8c714875
BLowdc <lowbh@dulwich.org.uk>
2024-09-07 09:56:17 +0100

    test sol.

--- assessments/y12_first_week/q6.py
+++ assessments/y12_first_week/q6.py
@@ -3,4 +3,7 @@ def high_1d(arr):
     for i in range(len(arr)):
         if arr[i] > high:
             high = arr[i]
-    print(high)
+    return high
+
+a = [int(s) for s in input().split()]
+print(high_1d(a))
--- assessments/y12_first_week/q7.py
+++ assessments/y12_first_week/q7.py
@@ -3,17 +3,16 @@ def high_1d(arr):
     for i in range(len(arr)):
         if arr[i] > high:
             high = arr[i]
-    print(high)
+    return high
 
 def big_2d(arr):
     big = arr[0][0]
     for row in arr:
-        print(arr)
         if high_1d(row) > big:
             big = high_1d(row)
     return big
 
 n = int(input())
 a = [[int(j) for j in input().split()] for i in range(n)]
-for row in a:
-    print(row)
+
+print(big_2d(a))
