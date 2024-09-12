920bf6f7d6c856228a9bbd5140b54ad0c6049c05
BLowdc <lowbh@dulwich.org.uk>
2024-09-12 08:35:23 +0100

    selection

---
+++ Programming/Selection/Hw1.py
@@ -0,0 +1,5 @@
+name = input()
+if name == 'Hazel':
+    print("Helloe Hazel")
+else:
+    print("We haven't met")
---
+++ Programming/Selection/Hw2.py
@@ -0,0 +1,4 @@
+temp = int(input())
+humid = int(input())
+if window == 'closed':
+    print("open the window")
