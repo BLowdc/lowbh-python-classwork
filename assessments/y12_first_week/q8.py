24b7fa0c2d5722823cbf92e3699f074f44d66fd2
BLowdc <lowbh@dulwich.org.uk>
2024-09-16 10:12:11 +0100

    update q8

--- assessments/y12_first_week/q8.py
+++ assessments/y12_first_week/q8.py
@@ -2,11 +2,9 @@ sen = input()
 i = 0
 len_sen = len(sen)
 num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-while i != len_sen:
+for i in sen:
     if sen[i] in '0123456789':
-        len_sen += (len(str(num[int(sen[i])])) - 1)
         left = sen[:i]
         right = sen[i+1:]
         sen = left + num[int(sen[i])] + right
-    i += 1
 print(sen)
