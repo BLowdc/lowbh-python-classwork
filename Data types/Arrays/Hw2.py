53e3d00282c297bfee6773f072f25635914fa449
BLowdc <lowbh@dulwich.org.uk>
2024-10-01 21:09:28 +0100

    fg

---
+++ Data types/Arrays/Hw2.py
@@ -0,0 +1,16 @@
+pupils = ['Brock Waters', 'Patrice Thornton','Refugio Vance'
+    ,'Blake Lozano','Francisco Barron','Alissa Jackson'
+    ,'Flossie Powers','Miriam Conner','Frieda Guerrero'
+    ,'Josue Webster','Lula Haley','Jarrod Joyce'
+    ,'Tyree Mack','Maude Duffy','Sang Mccullough'
+    ,'Lucile Torres','Lavern Henry','Alexandra Oneill'
+    ,'Carmella Farley','Mason Bender']
+print("GROUP 1: ")
+for i in range(0,19,2):
+    print(pupils[i])
+#next i
+print()
+print("GROUP 2:")
+for j in range(1,20,2):
+    print(pupils[j])
+#next i
