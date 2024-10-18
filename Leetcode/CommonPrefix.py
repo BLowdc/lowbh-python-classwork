cae20ade832a81e6f6439d7ce62ba954fabea115
BLowdc <lowbh@dulwich.org.uk>
2024-10-18 09:46:00 +0100

    snow

--- Leetcode/CommonPrefix.py
+++ Leetcode/CommonPrefix.py
@@ -1,17 +1,23 @@
 strs = ["flower","flower","flower","flower"]
 prefix = ""
 n = len(min(strs))
+skip = False
 index = 0
 if n == 0:
     print(prefix)
 elif len(strs) == 1:
     print(strs[0])
 else:
-    for s in range(1,len(strs)):
-        letter = strs[0][index]
-        if strs[s][index] != letter:
+    for l in range(n):
+        letter = strs[0][l]
+        for string in range(1,len(strs)):
+            if strs[string][l] != letter:
+                skip = True
+                break
+            else:
+                skip = False
+                continue               
+        if skip:
             break
-        else:
-            prefix += letter
-            index += 1
+        prefix += letter
     print(prefix)
--- Pygame/snow.py
+++ Pygame/snow.py
@@ -9,23 +9,32 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
+#record
+class Flake:
+    def __init__(self,x_pos,y_pos,velocity,size) -> None:
+        self.x = x_pos
+        self.y = y_pos
+        self.vel = velocity
+        self.size = size
+
 #default    
-size = (1000,800)
+size = (800,800)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Snow")
 done = False
 clock = pygame.time.Clock()
 pygame.key.set_repeat(50,50)
 
-sway = 0.05
-col = 4
-rows = 100
-arr = [[0 for i in range(col)]for j in range(rows)]
-for row in range(rows):  
-        arr[row][0] = random.randint(0,size[0]-3) # x-coord
-        arr[row][1] = random.randint(0,size[1]-3) # y-coord
-        arr[row][2] = arr[row][0] # x-coord constant for swaying
-        arr[row][3] = round(random.random(),2) # fall speed
+# sway = 0.05
+rows = 200
+arr = [None for j in range(rows)]
+for row in range(rows):
+        velocity = round(random.random(),2)
+        flake = Flake(random.randint(0,size[0]-3),
+                      random.randint(0,size[1]-3),
+                      velocity,
+                      velocity * 3 + 3)
+        arr[row] = flake
 
 while not done:
     for event in pygame.event.get():
@@ -35,24 +44,29 @@ while not done:
     #game logic
     for j in range(rows):
         #gravity
-        arr[j][1] += arr[j][3]
+        arr[j].y += arr[j].vel
         #sway
-        arr[j][0] += sway
+        # arr[j][0] += sway
         #LEFT RIGHT SWAY--------------
-        k = arr[j][2]
-        if arr[j][0] >= (k+10) or arr[j][0] <= (k-10):
-            sway *= -1
+        # k = arr[j][2]
+        # if arr[j][0] >= (k+10) or arr[j][0] <= (k-10):
+        #     sway *= -1
         #snow generation on top
-        if arr[j][1] >= (size[1]+3):
-            arr[j][0] = random.randint(0,size[0]-3)
-            arr[j][3] = round(random.random(),2)
-            arr[j][2] = arr[j][0]
-            arr[j][1] = -5              
+        if arr[j].y >= (size[1]+3):
+            velocity = round(random.random(),2)
+            flake = Flake(random.randint(0,size[0]-3),
+                          -5,
+                          velocity,
+                          velocity * 3 + 3)
+            arr[j] = flake
 
     screen.fill(BLACK)
     #drawing
-    for flake in range(rows):
-        pygame.draw.rect(screen,WHITE,[arr[flake][0],arr[flake][1],3,3])
+    for f in range(rows):
+        pygame.draw.rect(screen,WHITE,[arr[f].x,
+                                       arr[f].y,
+                                       arr[f].size,
+                                       arr[f].size])
     pygame.display.flip()
     clock.tick(120)
 pygame.quit()
