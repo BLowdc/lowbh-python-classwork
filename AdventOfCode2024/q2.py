1f26b4760b0bcccf81dfb36dc14bd089a55a272f
BLowdc <lowbh@dulwich.org.uk>
2024-12-04 09:57:23 +0000

    finalq2

--- AdventOfCode2024/q2.py
+++ AdventOfCode2024/q2.py
@@ -3,55 +3,13 @@ ls = []
 ls2 = []
 safe = 0
 
-def isIncreasing(list):
-    for i in range(1,len(list)):
-        if list[i] <= list[i-1]:
-            return False
-        elif (list[i] - list[i-1]) > 3:
-            return False
-        #end if
-    #next i
-    return True
-#end function
-
-def isDecreasing(list):
-    for i in range(1,len(list)):
-        if list[i] >= list[i-1]:
-            return False
-        elif (list[i-1] - list[i]) > 3:
-            return False
-        #end if
-    #next i
-    return True
-#end function
-
-def errorCount1(list):
-    error = 0
-    for i in range(1,len(list)):
-        if (list[i] >= list[i-1]) or ((list[i-1] - list[i]) > 3):
-            error += 1
-        #end if
-    #next i
-    if error == 1:
+def isSafe(levels):
+    differs = [a - b for a, b in zip(levels, levels[1:])]
+    is_monotonic = all(i > 0 for i in differs) or all(i < 0 for i in differs)
+    is_in_range = all(0 < abs(i) <= 3 for i in differs)
+    if is_monotonic and is_in_range:
         return True
-    else:
-        return False
-    #end if
-#end function
-
-def errorCount2(list):
-    error = 0
-    for i in range(1,len(list)):
-        if (list[i] <= list[i-1]) or ((list[i] - list[i-1]) > 3):
-            error += 1
-        #end if
-    #next i
-    if error == 1:
-        return True
-    else:
-        return False
-    #end if
-#end function
+    return False
 
 for line in f:
     a = [int(s) for s in line.strip().split()]
@@ -59,17 +17,13 @@ for line in f:
 #next line
 
 for level in ls:
-    if isIncreasing(level) or isDecreasing(level):
+    if isSafe(level):
         safe += 1
-    # else:
-    #     ls2.append(level)
-    #end if   
-#next level
-
-# for level in ls2:
-#     if errorCount1(s) or errorCount2(s):
-#         safe += 1
-#     #end if
-# #next level
-
+    else:
+        for i in range(len(level)):
+            tolerated_levels = level[:i] + level[i + 1 :]
+            if isSafe(tolerated_levels):
+                print(tolerated_levels)
+                safe += 1
+                break
 print(safe)
