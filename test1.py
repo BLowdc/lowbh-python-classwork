ec5806873d04599cac2120faee54bebb70a6dea7
BLowdc <lowbh@dulwich.org.uk>
2025-03-11 13:11:20 +0000

    l

--- test1.py
+++ test1.py
@@ -1,2 +1,12 @@
-for n in range(14,6,-1):
-    print(n)
+def toBinary(n) -> str:
+    quo = n
+    b = ''
+    while quo > 0:
+        rem = quo % 2
+        quo = quo // 2
+        b = str(rem) + b
+    return b
+
+print(toBinary(int(input())))
+
+
